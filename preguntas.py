2916133194b46221b1f1674c0fd2c65aef16dac0
karenrivgi <karenrivgi@gmail.com>
2024-02-26 17:54:55 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -6,6 +6,7 @@ Este archivo contiene las preguntas que se van a realizar en el laboratorio.
 
 Utilice los archivos `tbl0.tsv`, `tbl1.tsv` y `tbl2.tsv`, para resolver las preguntas.
 
+# attempt: 1
 """
 import pandas as pd;
 
